240beb532c20f23e18909a4b47ad9dd2d923d63d
Lynx1032 <71201272+Lynx1032@users.noreply.github.com>
2023-07-01 22:38:49 +0700

    Update QLLView.java

--- final_project/src/View/QLLView.java
+++ final_project/src/View/QLLView.java
@@ -1,4 +1,4 @@
-//package View;
+package View;
 
 import java.awt.CardLayout;
 import java.awt.Component;
@@ -26,7 +26,6 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
-/*
 import Controller.QLLNhanVienController;
 import Controller.QLLPhongBanController;
 import model.QLLModel;
@@ -39,7 +38,6 @@ import model.NhanVienThoiVu;
 import model.NhanVienThucTap;
 import model.NhanVienVanHanh;
 import model.NhanVienToanThoiGian;
-*/
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.SwingConstants;
